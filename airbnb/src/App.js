4ffd9b122814f8c747d8f68f9eb3d17589bf9c4c
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2020-04-15 20:06:47 -0500

    Inicio App: TemplateReact, Configuración inicial de Gulp, SASS y vscode Node Green

---
+++ airbnb/src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import './css/fontawesome-all.min.css';
+import './css/app.css';
+
+function App() {
+  return (
+    <Fragment>
+    </Fragment>
+  );
+}
+
+export default App;
