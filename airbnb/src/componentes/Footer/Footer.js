34ab116d9b08326366980c426fe487ba01128c1e
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2020-04-18 23:48:48 -0500

    Fin App: Creando botón para mostrar/ocultar footer, useEffect para ocultar Footer

--- airbnb/src/componentes/Footer/Footer.js
+++ airbnb/src/componentes/Footer/Footer.js
@@ -1,62 +1,105 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Footer = () => {
+
+    const [ boton, guardarBoton ] = useState({
+        clase: 'btn-flotante',
+        claseF: '',
+        texto: 'Idioma y Moneda'
+    });
+
+    const { clase, claseF, texto } = boton;
+
+    const onClick = (e) => {
+        e.preventDefault();
+
+        const claseMenu = e.target.classList;
+        // console.log(claseMenu);
+
+        if(claseMenu.contains('btn-flotante')) {
+
+            guardarBoton({
+                clase: 'activo',
+                claseF: 'activoF',
+                texto: 'X Cerrar'
+            });
+
+        } else {
+            
+            guardarBoton({
+                clase: 'btn-flotante',
+                claseF: '',
+                texto: 'Idioma y Moneda'
+            });
+            
+        }
+    };
+
     return (
-        <footer className="footer">
-            <div className="contenedor contenedor-cards">
-                <div className="nav-footer">
-                    <h3>MiViaje.com</h3>
-                    <nav className="menu">
-                        <a href="/">Empleo</a>
-                        <a href="/">Prensa</a>
-                        <a href="/">Politicas</a>
-                        <a href="/">Ayuda</a>
-                    </nav>
+        <Fragment>
+            <footer className={`footer ${claseF}`}> 
+                <div className="contenedor contenedor-cards">
+                    <div className="nav-footer">
+                        <h3>MiViaje.com</h3>
+                        <nav className="menu">
+                            <a href="/">Empleo</a>
+                            <a href="/">Prensa</a>
+                            <a href="/">Politicas</a>
+                            <a href="/">Ayuda</a>
+                        </nav>
+                    </div>
+                    <div className="nav-footer">
+                        <h3>Descubre MiViaje.com</h3>
+                        <nav className="menu">
+                            <a href="/">Confianza y Seguridad</a>
+                            <a href="/">Crédito de Viajero</a>
+                            <a href="/">AirBNB Citizen</a>
+                            <a href="/">Viaje de Negocios</a>
+                        </nav>
+                    </div>
+                    <div className="nav-footer">
+                        <h3>Hospedaje</h3>
+                        <nav className="menu">
+                            <a href="/">Razones para Hospedar</a>
+                            <a href="/">Hospitalidad</a>
+                            <a href="/">Ser un anfitrión responsable</a>
+                            <a href="/">Centro de la comunidad</a>
+                        </nav>
+                    </div>
+                    <div className="nav-footer">
+                        <nav className="sociales">
+                            <ul>
+                                <li>
+                                    <a href="http://facebook.com" rel="noopener noreferrer"></a>
+                                    <span>Facebook</span>
+                                </li>
+                                <li>
+                                    <a href="http://twitter.com"  rel="noopener noreferrer"></a>
+                                    <span>Twitter</span>
+                                </li> 
+                                <li>
+                                    <a href="http://instagram.com"  rel="noopener noreferrer"></a>
+                                    <span>Instagram</span>
+                                </li>
+                            </ul>
+                        </nav>
+                        <nav className="menu">
+                            <a href="/">Razones para Hospedar</a>
+                            <a href="/">Hospitalidad</a>
+                            <a href="/">Ser un anfitrión responsable</a>
+                            <a href="/">Centro de la comunidad</a>
+                        </nav>
+                    </div>
                 </div>
-                <div className="nav-footer">
-                    <h3>Descubre MiViaje.com</h3>
-                    <nav className="menu">
-                        <a href="/">Confianza y Seguridad</a>
-                        <a href="/">Crédito de Viajero</a>
-                        <a href="/">AirBNB Citizen</a>
-                        <a href="/">Viaje de Negocios</a>
-                    </nav>
-                </div>
-                <div className="nav-footer">
-                    <h3>Hospedaje</h3>
-                    <nav className="menu">
-                        <a href="/">Razones para Hospedar</a>
-                        <a href="/">Hospitalidad</a>
-                        <a href="/">Ser un anfitrión responsable</a>
-                        <a href="/">Centro de la comunidad</a>
-                    </nav>
-                </div>
-                <div className="nav-footer">
-                    <nav className="sociales">
-                        <ul>
-                            <li>
-                                <a href="http://facebook.com" rel="noopener noreferrer"></a>
-                                <span>Facebook</span>
-                            </li>
-                            <li>
-                                <a href="http://twitter.com"  rel="noopener noreferrer"></a>
-                                <span>Twitter</span>
-                            </li> 
-                            <li>
-                                <a href="http://instagram.com"  rel="noopener noreferrer"></a>
-                                <span>Instagram</span>
-                            </li>
-                        </ul>
-                    </nav>
-                    <nav className="menu">
-                        <a href="/">Razones para Hospedar</a>
-                        <a href="/">Hospitalidad</a>
-                        <a href="/">Ser un anfitrión responsable</a>
-                        <a href="/">Centro de la comunidad</a>
-                    </nav>
-                </div>
-            </div>
-        </footer>
+            </footer>
+            <a 
+                href="/" 
+                className={`${clase}`}
+                onClick={onClick}
+            >
+                {texto}
+            </a>
+        </Fragment>
     );
 }
 
--- airbnb/src/componentes/Hero/Hero.js
+++ airbnb/src/componentes/Hero/Hero.js
@@ -1,12 +1,62 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './../../img/logo.png';
+import arriba1 from './../../img/arriba.jpg';
+import arriba2 from './../../img/arriba2.jpg';
 
 const Hero = () => {
+
+    const hero = React.createRef();
+
+    useEffect(() => {
+        efectoImagen();
+    },[]);
+
+    const efectoImagen = () => {
+
+        console.log('efectoImagen');
+
+        const imagenHero = hero.current;
+        console.log('imagenHero', imagenHero);
+
+        let i = 0;
+        let tiempo = 0;
+
+        // Arreglo con imagenes de fondo
+        const imagenes = [arriba1, arriba2];
+
+        setInterval(() => {
+
+            if (tiempo > 40) {
+
+                tiempo = 0;
+                imagenHero.style.backgroundImage = `url(${imagenes[i]})`;
+
+                if(i === imagenes.length - 1) {
+                    i = 0;
+                } else {
+                    i++;
+                }
+                console.log(i);
+                console.log(imagenes[i]);
+
+            } 
+            
+            tiempo++;
+            imagenHero.style.backgroundPositionY = '-' + tiempo + 'px';
+
+        }, 100);
+
+    };
+
     return (
-        <div className="hero">
+        // <div className="hero" onSubmit={onSubmit}>
+        <div ref={hero} className="hero">
             <header className="header contenedor">
                 <div className="logo">
-                    <img src={logo} alt="logo"/>
+                    <img 
+                        src={logo} 
+                        alt="logo"
+                    />
                 </div>
                 <nav className="navegacion">
                     <a href="/">Vender</a>
